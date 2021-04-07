7776890bb5a7086952126b82539d1aae393a3811
DESKTOP-L56QVGG\Эльвир <mardanovelvir@gmail.com>
2021-04-07 23:25:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string score = "";
+            while (score != "exit")
+            {
+                    var options = new ConfigurationOptions();
+                options.EndPoints.Add("localhost", 6379);
+
+                var redis = ConnectionMultiplexer.Connect(options);
+                var db = redis.GetDatabase(3);
+
+                const string key1 = "appsKey";
+                db.KeyDelete(key1);
+                db.StringSet(key1, 1);
+
+                const string key2 = "appsKey";
+                db.KeyDelete(key2);
+                db.StringSet(key2, 4);
+
+                score = Console.ReadLine();
+            }
+        }
+    }
+}
